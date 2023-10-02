b2908c62ab8c1020e81e7dba161a70f9678e1a7a
A9d2i2Ps <mlgpikakuu@gmail.com>
2023-10-02 16:57:51 -0400

    Added free art assets Trying to create background has error will remake the use of class

--- mainGame.py
+++ mainGame.py
@@ -6,7 +6,7 @@ from win32api import GetSystemMetrics
 
 # Width of screen first height of screen second
 detail = [GetSystemMetrics(0),GetSystemMetrics(1)]
-detail = [1600,800]
+# detail = [1600,800]
 
 # At start of game so can function properly
 pygame.init()
@@ -20,8 +20,34 @@ text_font = pygame.font.Font("Font/Pixeltype.ttf")
 
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((200,100))
-test_surface.fill("pink")
+
+
+class assets():
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, width, height, image):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       print(image)
+       self.image = pygame.image.load(image)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+background_image = 'C:\\Users\\mlgpi\\Documents\\Code\\GameFor\\Images\\ocean.png'
+
+background = pygame.sprite.GroupSingle()
+background.add = assets(detail[0],detail[1],background_image)
+
+
+
+screen.blit(background_image,(detail[0],detail[1]))
 
 # Sets the title for the game when screen pops up
 pygame.display.set_caption('Bob')
@@ -38,6 +64,5 @@ while True:
             exit()
 
 
-    screen.blit(test_surface,(200,100))        
     pygame.display.update()
     clock.tick(60)
