4dfdadeda432baa82838862074b770014be15b79
A9d2i2Ps <mlgpikakuu@gmail.com>
2023-10-04 17:47:17 -0400

    Bob

--- mainGame.py
+++ mainGame.py
@@ -14,13 +14,13 @@ pygame.init()
 
 
 # Makes the screen that the game is played on
-screen = pygame.display.set_mode((detail[0],detail[1]))
+screen = pygame.display.set_mode((800,800))
 gameIcon = pygame.image.load("images/icon.png")
 text_font = pygame.font.Font("Font/Pixeltype.ttf")
 
 clock = pygame.time.Clock()
 
-background_surface = pygame.image.load('Images\ocean.png')
+background_surface = pygame.image.load('images/background.jpg')
 background_rect = background_surface.get_rect(topleft = (1,1))
 screen.blit(background_surface,background_rect)
 
@@ -28,6 +28,26 @@ screen.blit(background_surface,background_rect)
 pygame.display.set_caption('Bob')
 
 pygame.display.set_icon(gameIcon)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.draw.circle(screen,"purple",(400,400),20)
+        # self.rect = self.image.get_rect()
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self):
+        rect = self.rect
+        super().__init__()
+        self.image = pygame.draw.rect(screen,"gray",rect)
+        # self.rect = self.image.get_rect()
+
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
+wall = pygame.sprite.GroupSingle()
+wall.add(Wall())
 
 # Statement for running game
 while True:
